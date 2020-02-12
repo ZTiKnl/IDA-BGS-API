180802ff7def2a553a89e0aede2c52b95ee435c5
ZTiKnl <ZTiK.nl@gmail.com>
2020-02-12 23:04:45 +0100

    first commit

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+$securedbcreds = '../../private/db.inc.php';
+
+$loglocation = '../log/';
+$logapi = 'api-'.time().'.log';
+$logtickdetect = 'tickdetect-'.time().'.log';
+$logtickprocessor = 'tickprocessor-'.time().'.log';
+?>
