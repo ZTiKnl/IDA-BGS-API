2a50e9a6ff82f6d38f03048de42c7550ced8be97
ZTiKnl <ZTiK.nl@gmail.com>
2020-02-26 09:51:19 +0100

    added output-api log

--- config.inc.php
+++ config.inc.php
@@ -1,8 +1,19 @@
 <?php
+date_default_timezone_set("Europe/London");
+
 $securedbcreds = '../../private/db.inc.php';
 
 $loglocation = '../log/';
-$logapi = 'api-'.time().'.log';
+$logapi = 'inputapi-'.time().'.log';
+$logapiout = 'outputapi-'.time().'.log';
 $logtickdetect = 'tickdetect-'.time().'.log';
 $logtickprocessor = 'tickprocessor-'.time().'.log';
+
+$siteurl = 'https://ida-bgs.ztik.nl/';
+$sitetitle = 'Independent Defence Agency - BGS';
+
+$pmfname = 'Independent Defence Agency';
+$pmfshortname = 'IDA';
+
+$systeminfluencewarningpercentage = 1;
 ?>
