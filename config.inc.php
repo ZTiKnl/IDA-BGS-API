76230c79c7e23f5e82ba292c0c0cbc22e885a50c
ZTiKnl <ZTiK.nl@gmail.com>
2020-03-15 10:51:53 +0100

    rewrote logging

--- config.inc.php
+++ config.inc.php
@@ -4,10 +4,14 @@ date_default_timezone_set("Europe/London");
 $securedbcreds = '../../private/db.inc.php';
 
 $loglocation = '../log/';
-$logapi = 'inputapi-'.time().'.log';
-$logapiout = 'outputapi-'.time().'.log';
-$logtickdetect = 'tickdetect-'.time().'.log';
-$logtickprocessor = 'tickprocessor-'.time().'.log';
+$apiloginput = true;
+$logapiinput = '/input/api-input_'.time().'.log';
+$apilogoutput = true;
+$logapioutput = '/output/api-output_'.time().'.log';
+$apilogtickdetect = false;
+$logtickdetect = '/tickdetect/api-tickdetect_'.time().'.log';
+$apilogprocessor = true;
+$logtickprocessor = '/tickprocessor/api-tickprocessor_'.time().'.log';
 
 $siteurl = 'https://ida-bgs.ztik.nl/';
 $sitetitle = 'Independent Defence Agency - BGS';
